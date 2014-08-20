e7418f30d2ef8b8c03d4012cb8aaa2bce3a53fda
David Perez Millan <desarrollo@davidperezmillan.com>
2014-08-20 14:59:32 +0200

    Create GenericService.java

---
+++ services/GenericService.java
@@ -0,0 +1,10 @@
+package com.sandbox.sandbox.common.services;
+
+/**
+ * Clase abstracta de la que van a heredar todos las clases que sean servicios.
+ */
+public abstract class GenericService {
+
+    public abstract int execute();
+
+}
